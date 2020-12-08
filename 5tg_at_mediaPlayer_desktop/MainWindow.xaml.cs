b5424647f3c44f2e330e2165bb78fce0f85153b1
Shubham-Patil-SP <shubham60558@gmail.com>
2020-12-08 10:55:51 +0530

    worked on playing mp3 music

--- 5tg_at_mediaPlayer_desktop/MainWindow.xaml.cs
+++ 5tg_at_mediaPlayer_desktop/MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace _5tg_at_mediaPlayer_desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
         public MainWindow()
         {
             InitializeComponent();
+            Uri music = new Uri("C:\\Users\\shubh\\Desktop\\part time\\1st.mp3");
+            mediaPlayer.Open(music);
         }
 
         private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
@@ -69,6 +72,59 @@ namespace _5tg_at_mediaPlayer_desktop
         private void CloseBtn_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+
+        //play the media
+
+        private void PlayMedia_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            mediaPlayer.Play();
+            InitializePropertyValues();
+        }
+
+        private void PauseMedia_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        private void StopMedia_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+
+        private void VolumeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaPlayer.Volume = (double)volumeSlider.Value;
+        }
+
+        private void SpeedRatioSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            mediaPlayer.SpeedRatio = (double)speedRatioSlider.Value;
+        }
+
+        private void TimelineSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int SliderValue = (int)timelineSlider.Value;
+            TimeSpan ts = new TimeSpan(0, 0, 0, 0, SliderValue);
+            mediaPlayer.Position = ts;
+        }
+
+
+        void InitializePropertyValues()
+        {
+            mediaPlayer.Volume = (double)volumeSlider.Value;
+            mediaPlayer.SpeedRatio = (double)speedRatioSlider.Value;
+        }
+
+        private void MyMediaElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            timelineSlider.Maximum = mediaPlayer.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+        private void MyMediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
         }
     }
 }
