11f93866c5c4ac4525b38dbd535603289b45c738
Shubham-Patil-SP <shubham60558@gmail.com>
2021-01-01 11:13:03 +0530

    MediaInfoLib changes

--- 5tg_at_mediaPlayer_desktop/Playlist/Playlist.xaml.cs
+++ 5tg_at_mediaPlayer_desktop/Playlist/Playlist.xaml.cs
@@ -1,23 +1,12 @@
 ﻿using _5tg_at_mediaPlayer_desktop.connection;
-using _5tg_at_mediaPlayer_desktop.Bottom_Media_Control;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.VisualBasic.FileIO;
 using System.IO;
-using CsvHelper;
 
 namespace _5tg_at_mediaPlayer_desktop.Playlist
 {
